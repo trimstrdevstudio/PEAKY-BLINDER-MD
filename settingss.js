d9357fa8fed0084f06e099fd298044b35eac6a45
TRI MSTR DEV STUDIO <irokzmusicworld@gmail.com>
2025-10-19 00:03:58 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~sYhj1LRJ#Os2nFtAPuPdiZcMIqCxwWkBxkedmeqsHtXdyiCrSGtA",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
